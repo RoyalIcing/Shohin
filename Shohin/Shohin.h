bfb831634a4976e5a5e9ecbdcd211f270b529000
Patrick Smith <patrick@burntcaramel.com>
2018-05-14 21:55:38 +1000

    Initial Commit

---
+++ Shohin/Shohin.h
@@ -0,0 +1,19 @@
+//
+//  Shohin.h
+//  Shohin
+//
+//  Created by Patrick Smith on 14/5/18.
+//  Copyright © 2018 Royal Icing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shohin.
+FOUNDATION_EXPORT double ShohinVersionNumber;
+
+//! Project version string for Shohin.
+FOUNDATION_EXPORT const unsigned char ShohinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shohin/PublicHeader.h>
+
+
